562c429c77f805596e0b8fa0b78f99edc71d0049
Grace Pearcey <pearcey.grace@gmail.com>
2023-02-24 16:05:00 -0330

    not done, rewriting nodes in cpp for melodic

---
+++ gate-guider_ws/src/navigation/src/fake_bbox_angles.cpp
@@ -0,0 +1,29 @@
+#include <ros/ros.h>
+#include <navigation/Prop.h>
+
+void angle_range_finder() {
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<navigation::Prop>("prop_angle_range", 1);
+    ros::Rate rate(10);
+    navigation::Prop msg;
+    msg.prop_type = "red_marker";
+    msg.theta_1 = 177;
+    msg.theta_2 = 4;
+
+    while (ros::ok()) {
+        ROS_INFO_STREAM(msg);
+        pub.publish(msg);
+        rate.sleep();
+    }
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "angle_range_finder_node");
+    try {
+        angle_range_finder();
+    }
+    catch (ros::Exception& e) {
+        ROS_ERROR_STREAM("An exception occurred in the angle_range_finder_node: " << e.what());
+    }
+    return 0;
+}
